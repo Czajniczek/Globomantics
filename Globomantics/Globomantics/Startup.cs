e5cbb79f36392d57cd4d851724ba8c9f26c5a00e
Gabrielos1 <gabrielos123456789@gmail.com>
2021-07-15 22:04:59 +0200

    Application Settings
    Application Settings

--- Globomantics/Globomantics/Startup.cs
+++ Globomantics/Globomantics/Startup.cs
@@ -2,6 +2,7 @@ using Globomantics.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,6 +15,13 @@ namespace Globomantics
     // Klasa konfiguruj¹ca aplikacjê
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 
@@ -37,6 +45,8 @@ namespace Globomantics
             // Za ka¿dym razem, gdy jakiœ typ prosi o obiekt IConferenceService, podaj wyst¹pienie ConferenceApiService
             services.AddSingleton<IConferenceService, ConferenceMemoryService>();
             services.AddSingleton<IProposalService, ProposalMemoryService>();
+
+            services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
